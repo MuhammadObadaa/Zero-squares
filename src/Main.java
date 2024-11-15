25b4741cd94f4724ebe0c3c63d51639023be70d6
Muhammad Obadaa Almasri <obadaa.masri@gmail.com>
2024-11-15 15:59:01 +0300

    Implementing DFS search in the FullSearchController

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-import constants.MoveDirection;
-import controllers.StateController;
+import controllers.FullSearchController;
+import controllers.GameController;
 import models.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
 
         Game game = new Game("C:/lv2.zs");
 
-        StateController stateController = new StateController(game);
+        GameController stateController = new GameController(game);
+
+        FullSearchController f = new FullSearchController(game.getState());
+        int i = 0;
+        ArrayList<State> s = f.DFSSearch();
+        System.out.println(s.size());
+
+        for (State state : s) {
+            System.out.println(++i);
+            state.print();
+        }
 
         game.show();
 
