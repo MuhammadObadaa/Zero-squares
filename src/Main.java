7f3ef6bad317330c69b85005aabfe57e69d2fdc9
Muhammad Obadaa Almasri <obadaa.masri@gmail.com>
2024-11-16 16:09:10 +0300

    Beta game console UI

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,87 @@ import models.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws CloneNotSupportedException {
+    public static void main(String[] args) throws CloneNotSupportedException, IOException {
+        chooseAndPick();
+    }
 
-        Game game = new Game("./resources/levels/lv20.zs");
+    public static void chooseAndPick() throws IOException {
 
-        GameController stateController = new GameController(game);
+        int in = 3;
 
-        FullSearchController f = new FullSearchController(game.getState());
-        int i = 0;
-        long start = System.currentTimeMillis();
-        ArrayList<State> s = f.BFSSearch();
-        long end = System.currentTimeMillis();
-
-        System.out.println((end - start)/1000.0);
-
-        for (State state : s) {
-            System.out.println(++i);
-            state.print();
+        while(in != 1 && in != 2){
+            System.out.println("Choose between:\n1). Let the AI play\n2). Let me play");
+            in = (new Scanner(System.in)).nextInt();
         }
 
-        while(!game.finished()){
+        if(in == 1){
+            AIPlay();
+        }else{
+            HumanPlay();
+        }
+    }
+
+    public static void AIPlay(){
+        Game game;
+        FullSearchController controller;
+        for (int i = 1; i <= 20; i++) {
+
+            String filepath = "./resources/levels/lv" + String.format("%02d", i) + ".zs";
+
+            game = new Game(filepath);
 
             game.show();
 
+            /// BFS:
+            controller = new FullSearchController(game.getState());
+
+            System.out.println("\n\t\tBFS Approach:");
+
+            long start = System.nanoTime();
+            ArrayList<State> path = controller.BFSSearch();
+            long end = System.nanoTime();
+
+            System.out.printf("Execution Time:%.10f sec\n",(end - start)/1000000000.0);
+            System.out.println("Found path long:" + path.size());
+
+            /// DFS:
+            controller = new FullSearchController(game.getState());
+
+            System.out.println("\n\t\tDFS Approach:");
+
+            start = System.nanoTime();
+            path = controller.DFSSearch();
+            end = System.nanoTime();
+
+            System.out.printf("Execution Time:%.10f sec\n",(end - start)/1000000000.0);
+            System.out.println("Found path long:" + path.size());
+        }
+    }
+
+    public static void HumanPlay(){
+        int i = 1;
+        Game game = new Game("./resources/levels/lv" + String.format("%02d", i) + ".zs");
+        GameController controller = new GameController(game);
+
+
+
+        while(!game.finished()){
+            game.show();
+
             try {
-                stateController.control();
+                controller.control();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-
         }
 
         System.out.println("Winner");
+
+        if(i != 20){
+            HumanPlay();
+        }
     }
 }
