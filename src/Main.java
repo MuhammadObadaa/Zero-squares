3d8d7a5ae4f116e389599461b6349ba02eda4b84
Muhammad Obadaa Almasri <obadaa.masri@gmail.com>
2024-11-16 07:37:53 +0300

    Add levels into project folder

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,17 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
 
-        Game game = new Game("C:/lv19.zs");
+        Game game = new Game("./resources/levels/lv05.zs");
 
         GameController stateController = new GameController(game);
 
         FullSearchController f = new FullSearchController(game.getState());
         int i = 0;
+        long start = System.currentTimeMillis();
         ArrayList<State> s = f.BFSSearch();
-        System.out.println(s.size());
+        long end = System.currentTimeMillis();
+
+        System.out.println((end - start)/1000.0);
 
         for (State state : s) {
             System.out.println(++i);
